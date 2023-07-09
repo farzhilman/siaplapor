b10e0c4a287bf17c41243b332568a54eefb88566
farizh <farerich2@gmail.com>
2023-07-09 10:41:07 +0700

    xlsx

--- application/views/dashboard_bersih_excel.php
+++ application/views/dashboard_bersih_excel.php
@@ -18,7 +18,7 @@ table > tbody > tr > td{
 <body>
   <?php
   header("Content-type: application/vnd-ms-excel");
-  header('Content-Disposition: attachment; filename="'.$nama_file.'.xls"');
+  header('Content-Disposition: attachment; filename="'.$nama_file.'.xlsx"');
   ?>
 
   <div class="fullcontent_bersih">
