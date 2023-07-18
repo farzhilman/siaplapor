9f57ed3858fef5829e4cc453539396a22793d3df
farizh <farerich2@gmail.com>
2023-07-18 09:36:14 +0700

    fix bug. hide menu user

--- application/views/components/1/user_menu.php
+++ application/views/components/1/user_menu.php
@@ -9,14 +9,14 @@
     <li class="nav-item">
       <a href="<?=base_url('dashboard/admin')?>" class="nav-link">Report</a>
     </li>
-    <li class="nav-item dropdown">
+    <!-- <li class="nav-item dropdown">
       <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Pengaturan</a>
       <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
         <li>
           <a href="<?=base_url('dashboard/setting/user')?>" class="dropdown-item">User</a>
         </li>
       </ul>
-    </li>
+    </li> -->
     <!-- <li class="nav-item dropdown">
       <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Dropdown</a>
       <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
--- application/views/contents/entry/_form.php
+++ application/views/contents/entry/_form.php
@@ -32,13 +32,9 @@
                 <?php endforeach;?>
             </select>
         </div>
-        <div class="form-group">
-            <label for="alamat">Alamat<code>*</code>:</label>
-            <input type="text" class="form-control form-control-border border-width-2" id="alamat" name='alamat' placeholder="Input alamat" <?php if(!empty($entri)) {?>value="<?=$entri['alamat']?>" <?php }?>>
-        </div>
         <!-- Date and time -->
         <div class="form-group">
-            <label>Tanggal & Jam Rencana Kunjungan<code>*</code>:</label>
+            <label>Tanggal Giat<code>*</code>:</label>
             <div class="row">
               <div class="col-md-3">
                 <div class="input-group date" id="tanggal" data-target-input="nearest">
@@ -59,6 +55,10 @@
                   </div>
               </div> -->
             </div>
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat<code>*</code>:</label>
+            <input type="text" class="form-control form-control-border border-width-2" id="alamat" name='alamat' placeholder="Input alamat" <?php if(!empty($entri)) {?>value="<?=$entri['alamat']?>" <?php }?>>
         </div>
         <div class="row">
           <div class="col-md-4">
@@ -145,5 +145,72 @@
         format: 'HH:mm'
       });
       bsCustomFileInput.init();
+    });
+
+  $('#form-kunjungan').validate({
+        rules: {
+          petugas: 'required',
+          seksi: 'required',
+          giat: 'required',
+          tanggal: 'required',
+          alamat: 'required',
+          rw: 'required',
+          rt: 'required',
+          tinjau: 'required',
+          saran: 'required',
+          tindaklanjut: 'required',
+          dokumentasi: {
+                required: true,
+                extension: "JPG|jpg|JPEG|jpeg|PNG|png",
+            },
+        },
+        messages: {
+          petugas: "Mohon untuk mengisi nama Petugas.",
+          seksi: "Mohon untuk memilih Seksi.",
+          giat: "Mohon untuk memilih Dasar Giat.",
+          tanggal: "Mohon untuk mengisi Tanggal.",
+          alamat: "Mohon untuk mengisi Alamat.",
+          rw: "Mohon untuk memilih RW.",
+          rt: "Mohon untuk memilih RT.",
+          tinjau: "Mohon untuk mengisi Hasil Tinjau Lokasi.",
+          saran: "Mohon untuk mengisi Saran Masukan.",
+          tindaklanjut: "Mohon untuk mengisi Tindak Lanjut.",
+          dokumentasi: {
+            required: "Mohon untuk mengupload foto.",                  
+            extension: "File harus foto.",
+          },
+        },
+        submitHandler: function(form) {
+            Swal.fire({
+          showDenyButton: true,
+          showCloseButton: true,
+          confirmButtonText: 'Sesuai',
+          denyButtonText: 'batal',
+              icon: 'warning',
+              title:'Submit',
+              text: 'Apa anda yakin laporan telah sesuai ?'
+          }).then((result) => {
+          if (result.isConfirmed) {
+          $("#form-kunjungan").ajaxSubmit(function(data){
+            $(".isi-form").html("<i class='fa fa-spin fa-spinner'></i> Mohon Tunggu");;
+            // $(".keterangan").html('Terima Kasih atas kesediaan saudara/i dalam mengisi form ini.');
+            $(".isi-form").html(data);
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+          });
+          } else if (result.isDenied) {
+          }
+        })
+        },
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+          error.addClass('invalid-feedback');
+          element.closest('.form-group').append(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+          $(element).addClass('is-invalid');
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $(element).removeClass('is-invalid');
+        }
     });
 </script>
